b59c46f0403f70550d84e2691ded94313fd2ecbe
shifterest <86647101+shifterest@users.noreply.github.com>
2026-03-05 09:31:13 +0800

    ugh

--- frontend/flutter/lib/config/config.dart
+++ frontend/flutter/lib/config/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String apiBaseUrl = 'https://api.attendance.whatta.top';
+  static const String apiBaseUrl = 'https://attendance.whatta.top/api';
   static const String rpId = 'attendance.whatta.top';
   static const String rpName = 'Passkey Attendance System';
 
